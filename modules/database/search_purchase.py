ea9b42e3196e546d97d8ee859bc20ee56cc17e45
NexusAOD <samuel.espinoza0177@alumnos.udg.mx>
2024-04-06 16:10:01 -0600

    archivos carpeta database

---
+++ modules/database/search_purchase.py
@@ -0,0 +1,19 @@
+"""
+    Este archivo contiene los comportamientos de las consultas que haremos
+    a la base de datos en la entidad 'busco_compro.
+"""
+
+from modules.database.connection import get_connection
+
+def new_search_purchase(ID_user: int, status: str, description: str, date: str, title: str) -> bool:
+
+    agora = get_connection()
+   
+    with agora.cursor() as cursor:
+        cursor.execute("INSERT INTO busco_compro(ID_Usuario, Estado, Descripcion, Fecha, Titulo) VALUES (%s, %s, %s, %s, %s)", 
+                       (ID_user, status, description, date, title))
+    
+    agora.commit()
+    agora.close()
+
+    return True
